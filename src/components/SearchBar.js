235df9c0507deed584baf7f9ee955c0d3fbbac32
Brendan McKeen <bmckeen707@gmail.com>
2023-09-25 20:57:48 -0700

    searchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+function SearchBar() {
+    return (
+        <div>
+            <form>
+                <input></input>
+                <button type="submit" >Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar;
